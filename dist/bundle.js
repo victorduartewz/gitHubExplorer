1561e7c12d089b0a907596b2d08e3f67fca5e14f
victorduartewz <contato.vhzborges@gmail.com>
2025-09-30 00:19:49 -0300

    add-babel-introduction

---
+++ dist/bundle.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var _react = _interopRequireDefault(require("react"));
+function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
+function App() {
+  return /*#__PURE__*/_react["default"].createElement("h1", null, "Hello World");
+}
